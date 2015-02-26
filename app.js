849e8ba28750707e5ee50f80ef71a6403c1dd928
Weiwei SUN <ww.sun@outlook.com>
2015-02-26 16:45:45 +0800

    project structure

--- app.js
+++ app.js
@@ -2,19 +2,18 @@
 var mongoose = require('mongoose');
 var express = require('express');
 
+var routes = require('./routes');
+
 mongoose.connect('mongodb://localhost/blog', function(err) {
     if (err) throw err;
     console.log('connected!');
 
     var app = express();
+    var port = 3000;
 
-    // default route test, delete it next
-    app.get('/', function(req, res){ //Handle the user routes
-        res.send(200, 'hello mongoose blog');
-    });
+    routes(app); // app router
 
-    app.listen(3000, function() {
-        console.log('now listening on http://localhost:3000');
-
+    app.listen(port, function() {
+        console.log('The server is running on http://localhost:%s', port);
     });
 });
