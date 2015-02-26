bc350a9da06b051ab061df17f37615fc14943982
Weiwei SUN <ww.sun@outlook.com>
2015-02-26 17:02:04 +0800

    error handler

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 
+var errors = require('./errors');
+
 module.exports = function (app) {
 
     // home page
     app.get('/', function (req, res) {
         res.render('home.jade');
-    })
+    });
+
+
+    // error handlers
+    errors(app);
 }
