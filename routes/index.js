62c2abb31c7d9394f1b5921c267cfa95a0405853
Weiwei SUN <ww.sun@outlook.com>
2015-03-01 19:14:04 +0800

    routes for blog app

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+var errors = require('./errors');
+var login = require('./login');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var posts = require('./posts');
+var mongoose = require('mongoose');
 
-module.exports = router;
+var BlogPost = mongoose.model('BlogPost');
+
+module.exports = function (app) {
+
+    // home page
+    app.get('/', function (req, res) {
+        BlogPost.find().sort('created').limit(10).exec(function (err, posts) {
+            if (err) return next(err);
+            res.render('index', { posts: posts });
+        })
+    });
+
+    // login / logout routes
+    login(app);
+
+    // blog post crud
+    posts(app);
+
+    // error handlers
+    errors(app);
+}
